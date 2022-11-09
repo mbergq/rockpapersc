07f683de39d384ec87d8067a863a6bd630b44d2b
Martin <106612709+menkodot@users.noreply.github.com>
2022-11-09 19:12:23 +0100

    computer responds to clicks

--- script.js
+++ script.js
@@ -14,11 +14,18 @@ const scissors = document.createElement('button');
 scissors.classList.add('scissors');
 scissors.textContent = "Scissors";
 container.appendChild(scissors);
-//hey
+
 container.addEventListener('click', function (e) {
-    console.log(e.currentTarget.id);
+    console.log(e.target);
+    
     if (e.target == rock) {
-        console.log("Yep u clicked on rock");
+        console.log("You chose Rock!");
+    }
+    else if (e.target == paper) {
+        console.log("You chose Paper!")
+    }
+    else if (e.target == scissors) {
+        console.log("You chose Scissors!");
     }
     else {
         console.log("no");
